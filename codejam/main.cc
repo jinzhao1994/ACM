05dee56314c3426a7886fcaf736e0d901497f0e3
jinzhao1994 <jinzhao1994@126.com>
2018-04-08 16:57:51 +0800

    Add codejam new template.

---
+++ codejam/main.cc
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+// TODO: judge this code
+
+int main() {
+    int total_cas;
+    scanf("%d", &total_cas);
+    for (int cas = 1; cas <= total_cas; cas++) {
+        printf("Case #%d: IMPOSSIBLE\n", cas);
+    }
+    return 0;
+}
